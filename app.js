2dedea4126fbb67456500a533bb227ac6492931f
Shovon Das <shovondas26@gmail.com>
2020-12-14 11:02:22 +0600

    port updated

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const router = require('./router/router');
 const {getUser, getUserInRoom, addUser, removeUser} = require('./users');
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5200;
 
 const app = express();
 const server = http.createServer(app);
